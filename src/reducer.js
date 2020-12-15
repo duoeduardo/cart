ab9cf532642546f54af1a2804569be48faec5ee4
Eduardo <duoeduardo@gmail.com>
2020-12-15 19:02:00 -0300

    agrega aumentar disminuir cant

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,39 @@
-const reducer =(state, action) => {
-    //Se pueden usar switch o varios if
-    if (action.type === 'CLEAR_CART') {
-        return {...state, cart:[]}
-    }
+import { act } from "react-dom/test-utils";
 
+const reducer = (state, action) => {
+  //Se pueden usar switch o varios if
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
 
-    return state
-}
+    return { ...state, cart: tempCart };
+  }
 
-export default reducer
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount - 1 };
+      }
+      return cartItem;
+    });
+
+    return { ...state, cart: tempCart };
+  }
+
+  return state;
+};
+
+export default reducer;
